a9ed009ee7a390c300b6f58ef8cd254c884cd673
darshan.mistry <darshan.mistry@softwebsolutions.com>
2019-04-01 19:45:13 +0530

    Kotlin examples.

--- src/main/HigherOrderFunctions.kt
+++ src/main/HigherOrderFunctions.kt
@@ -6,9 +6,27 @@ fun main() {
     function1(20, onResponse = {
         println("This is function1")
     })
+
+    function2(25, onResponse = {
+        println("Value is $it")
+    })
+
+    val strValue = function3(51) {
+        println("Function3 $it")
+        return@function3 "Darshan"
+    }
+    println(strValue)
 }
 
 private fun function1(num: Int, onResponse: () -> Unit) {
     println("------------------------${num}")
     onResponse.invoke()
+}
+
+private fun function2(num: Int, onResponse: (Int) -> Unit) {
+    onResponse(num)
+}
+
+private fun function3(num: Int, onResponse: (Int) -> String) {
+    onResponse(num)
 }
